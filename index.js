6be5e10c6f7e27f251591cd43f2d8dea57a88dce
Brandon Parmley <orem.bparmley@gmail.com>
2016-02-08 09:49:05 -0700

    made the api endpoints

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var port = 8000;
 
 // write the API here
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
+app.get("/api", function (req, res) {
 
+    res.json({
+        "data": "here is some"
+    });
 
+});
+
+app.get("/api/data", function (req, res) {
+
+    res.json({
+        "data": 'yah... this is data'
+    });
+
+});
+
+app.post("/api/data", function (req, res) {
+
+    console.log(req.body);
+    res.json({
+        query: req.body.data
+    });
+
+});
+     
 //////////////////////////////////////////
 ///       No need to edit below :)     ///
 //////////////////////////////////////////
@@ -20,9 +49,8 @@ app.listen(port, function(err){
     if(err){
         //if there is, log it
         console.log(err);
+    } else {
+        //otherwise tell us that it is running
+        console.log('Magic is happening on port ' + port);
     }
-
-    //otherwise tell us that it is running
-    console.log('Magic is happening on port ' + port);
-
 });
